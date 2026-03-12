64186c9bac0ca02e3d17b091f5a21d7f0ba6621d
Luca Amoriello <luca.amoriello@hotmail.it>
2026-03-12 21:07:39 +0100

    Add debug logs

--- custom_components/ooni_connect/coordinator.py
+++ custom_components/ooni_connect/coordinator.py
@@ -31,6 +31,20 @@ class OoniConnectCoordinator(DataUpdateCoordinator[Any]):
     def _handle_bluetooth_update(self, data: Any) -> None:
         """Callback für Datenpakete."""
         _LOGGER.debug("Ooni Daten empfangen: %s", data)
+
+        # DEBUG: Logging für alle empfangenen Daten
+        _LOGGER.info(">>> BLUETOOTH UPDATE EMPFANGEN <<<")
+        _LOGGER.info("Data ist None: %s", data is None)
+        _LOGGER.info("Data type: %s", type(data).__name__)
+
+        if data is not None:
+            _LOGGER.info("Data repr: %s", repr(data))
+            if hasattr(data, '__dict__'):
+                _LOGGER.info("Data.__dict__: %s", data.__dict__)
+            _LOGGER.info("Dir(data): %s", [x for x in dir(data) if not x.startswith('_')])
+
+        _LOGGER.info(">>> ENDE BLUETOOTH UPDATE <<<")
+
         self._last_data = data
         self.async_set_updated_data(data)
 
@@ -45,18 +59,25 @@ class OoniConnectCoordinator(DataUpdateCoordinator[Any]):
         if self.client is None or not self.client.is_connected:
             if self._connection_task is None or self._connection_task.done():
                 self._connection_task = self.hass.async_create_task(self._connect_in_background())
-        
+
         return self._last_data
 
     async def _connect_in_background(self) -> None:
         """Versucht die Verbindung im Hintergrund aufzubauen, ohne HA zu blockieren."""
-        from ooni_connect_bluetooth.client import Client
+        try:
+            from ooni_connect_bluetooth.client import Client
+        except ImportError as import_err:
+            _LOGGER.error("Kann ooni_connect_bluetooth nicht importieren: %s", import_err)
+            return
 
         async with self._lock:
+            _LOGGER.debug("Versuche BLE Gerät mit Adresse %s zu finden...", self.address)
             ble_device = async_ble_device_from_address(self.hass, self.address)
             if not ble_device:
-                _LOGGER.debug("Ooni nicht erreichbar (Hintergrund)")
+                _LOGGER.debug("Ooni Gerät nicht erreichbar mit Adresse: %s", self.address)
                 return
+
+            _LOGGER.info("BLE Gerät gefunden: %s", ble_device.name or ble_device.address)
 
             try:
                 _LOGGER.info("Hintergrund-Verbindung zu Ooni wird aufgebaut...")
@@ -67,7 +88,12 @@ class OoniConnectCoordinator(DataUpdateCoordinator[Any]):
                         notify_callback=self._handle_bluetooth_update,
                         disconnected_callback=self._on_disconnected
                     )
-                _LOGGER.info("Ooni im Hintergrund erfolgreich verbunden")
+                _LOGGER.info("✓ Ooni im Hintergrund erfolgreich verbunden")
+                _LOGGER.info("Client ist: %s", self.client)
+                _LOGGER.info("Notify callback registriert: %s", self._handle_bluetooth_update)
+            except asyncio.TimeoutError:
+                _LOGGER.error("Timeout beim Verbinden mit Ooni (20 Sekunden)")
+                self.client = None
             except Exception as err:
-                _LOGGER.debug("Hintergrund-Verbindung fehlgeschlagen (Gerät wohl aus): %s", err)
+                _LOGGER.error("Hintergrund-Verbindung fehlgeschlagen. Fehler: %s (%s)", err, type(err).__name__, exc_info=True)
                 self.client = None
